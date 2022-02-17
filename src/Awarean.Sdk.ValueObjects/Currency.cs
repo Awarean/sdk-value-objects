a3b33f6c00676a3b8171511217fc8dae0d7a58f3
Brenon Ortega <bortega@acesso.com>
2022-02-17 19:44:52 -0300

    first currency wrong implementation, moves workflow to correct folder

--- src/Awarean.Sdk.ValueObjects/Currency.cs
+++ src/Awarean.Sdk.ValueObjects/Currency.cs
@@ -16,18 +16,53 @@ namespace Awarean.Sdk.ValueObjects
         public Currency(string code, double amount)
         {
             Code = ThrowIfInvalid(code);
-            amount = 
+            this.amount = ValidAmount(amount);
         }
 
-        private string ThrowIfInvalid(string code) => code is not null && code.Length == 3
+        public Currency(string code, decimal amount)
+        {
+            Code = ThrowIfInvalid(code);
+            this.amount = ValidAmount(amount);
+        }
+
+        public Currency(string currency, long cents)
+        {
+            Code = ThrowIfInvalid(currency);
+            this.amount =  ValidAmount(cents);
+        }
+
+        private static string ThrowIfInvalid(string code) => code is not null && code.Length == 3
                 ? code
                 : throw new ArgumentException("Currency code should have three uppercase characters corresponding to an existing currency.", nameof(code));
 
-        private long ValidAmount(long amount) => amount > 0 ? amount : throw new ArgumentException("Currency amount should be greather than 0.");
+        private static long ValidAmount(long amount) => amount > 0 ? amount : throw new ArgumentException("Currency amount should be greather than 0.");
 
-        private long ValidAmount(decimal amount) => ValidAmount(DecimalToLong(amount));
+        private static long ValidAmount(decimal amount) => ValidAmount(DecimalToLong(amount));
 
-        private double AmountAsDouble() => Convert.ToDouble(amount) / 100;
-        private long DecimalToLong(decimal amount) => this.amount = Convert.ToInt64(amount * 100);
+        private static long ValidAmount(double amount) => ValidAmount(DoubleToLong(amount));
+                
+        private double AmountAsDouble() => Convert.ToDouble(amount / 100);
+        private static long DecimalToLong(decimal amount) => ConvertToLong(amount);
+        private static long DoubleToLong(double amount) => ConvertToLong(amount);
+
+        private static long ConvertToLong<T>(T value) where T : struct
+        {
+            var multiplier = 100;
+            object casted = null;
+
+            if(value is decimal decimalValue)
+                casted = decimalValue * multiplier;
+
+            if (value is double doubleValue)
+                casted = doubleValue * multiplier;
+
+            if(casted is null)
+                throw new ArgumentException("Value should be a double or decimal."); 
+
+            return Convert.ToInt64(casted);
+        }
+
+        public static implicit operator double(Currency currency) => currency.AmountAsDouble();
+        public static implicit operator decimal(Currency currency) => Convert.ToDecimal(currency.AmountAsDouble());
     }
 }
